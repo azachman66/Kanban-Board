f09af4ae0f274ed8e8303a10481e13df70bec535
Aspen Zachman <azachman66@gmail.com>
2025-02-04 13:45:34 -0500

    Update authAPI.tsx

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,1 +1,10 @@
+import { UserLogin } from "../interfaces/UserLogin";
+
+const login = async (userInfo: UserLogin) => {
+  // TODO: make a POST request to the login route
+}
+
+
+
+export { login };
 
